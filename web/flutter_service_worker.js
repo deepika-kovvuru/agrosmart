1b26d8aafbe974fb25fd878ea984c3c7534a94b8
deepika-kovvuru <deepika-kovvuru@users.noreply.github.com>
2026-08-16 15:14:48 +0530

    Deploy Flutter Web for GitHub Pages with base-href /agrosmart/ and .nojekyll

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "cd6ce87477fd36649ee23377249b5de5",
+"flutter_bootstrap.js": "779aaf71e5685cc8954fbfd5eab5c262",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "81036c095c7303500ae3f8d5f2076340",
 "/": "81036c095c7303500ae3f8d5f2076340",
-"main.dart.js": "28c6ab281f1e20757c107767d76294bb",
+"main.dart.js": "29cafafee1494a710f110a608d4b7ffc",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
