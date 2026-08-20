15250febc785c3a9ead7adce9d5bc095789a395b
deepika-kovvuru <deepika-kovvuru@users.noreply.github.com>
2026-08-20 10:03:59 +0530

    Fix state and district text visibility with high-contrast Indian state selection strip

--- backend/static/flutter_service_worker.js
+++ backend/static/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9f767767102d4306737cde9e3bf0629b",
+"flutter_bootstrap.js": "d256299856a7f84f72a7112ecceef41b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "493caa355267e763209af67ddbb602a9",
 "/": "493caa355267e763209af67ddbb602a9",
-"main.dart.js": "9f1b1bf3788afe97dd0822c2d2a16f8b",
+"main.dart.js": "77adad9a4ff108e72bbfa862e99166e1",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"};
 // The application shell files that are downloaded before a service worker can
